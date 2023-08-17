d1b057d623b5830b60f675339386f7b91c601f04
romanstas20 <roman.stasyuk.work@gmail.com>
2023-08-18 01:56:25 +0300

    Initial commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
-import './globals.css'
+import React from "react";
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import AuthRequired from "@/components/authRequired";
+import './globals.css'
 
 const inter = Inter({subsets: ['latin']})
 
